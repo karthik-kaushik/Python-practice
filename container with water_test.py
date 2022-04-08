a2fda91181aea2902f24339fa02d1e98ebaba475
karthik-kaushik <97777957+karthik-kaushik@users.noreply.github.com>
2022-04-08 00:06:13 -0400

    test

--- container with water_test.py
+++ container with water_test.py
@@ -1,15 +1,17 @@
+
+import unittest
+
 from container_with_water import Solution
 
-def regular_test():
-    Test=[1,1]
-    assert Solution().maxArea(Test)==1,"should be 1"
+class Testing(unittest.TestCase):
+    def test_Regular(self):
+        Test=[1,1]
+        assert Solution().maxArea(Test)==1,"should be 1"
 
-def edge_test():
-    Test=[1,8,6,2,5,4,8,3,7]
-    assert Solution().maxArea(Test)==49,"should be 49"
+    def test_edge(self):
+        Test=[1,8,6,2,5,4,8,3,7]
+        assert Solution().maxArea(Test)==49,"should be 49"
 
 
 if __name__ == "__main__":
-    regular_test()
-    edge_test()
-    print("pass")
+    unittest.main()
--- template.py
+++ template.py
@@ -1,20 +1,24 @@
 import unittest
 
 class Solution:
+    pass
 
 
+----------------------------------------------------------------
 
+import unittest
 
-def regular_test():
-    Test=[1,1]
-    assert Solution().maxArea(Test)==1,"""should be """
+from container_with_water import Solution
 
-def edge_test():
-    Test=[1,8,6,2,5,4,8,3,7]
-    assert Solution().maxArea(Test)==49,"""should be """
+class Testing(unittest.TestCase):
+    def test_Regular(self):
+        Test=[1,1]
+        assert Solution().maxArea(Test)==1,"should be 1"
+
+    def test_edge(self):
+        Test=[1,8,6,2,5,4,8,3,7]
+        assert Solution().maxArea(Test)==49,"should be 49"
 
 
 if __name__ == "__main__":
-    regular_test()
-    edge_test()
-    print("pass")
+    unittest.main()
